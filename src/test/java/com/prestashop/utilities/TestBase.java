fdeef944553d1048b45db888a35e2299f26bc745
ozturktx <Trabzon6141>
2018-12-31 20:59:16 -0600

    FunctionalTestCasesAdded

--- src/test/java/com/prestashop/utilities/TestBase.java
+++ src/test/java/com/prestashop/utilities/TestBase.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.asserts.SoftAssert;
+
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
